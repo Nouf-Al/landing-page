88c2f29c47198ce52fec392d4e1073792a474275
Nouf <nouffawazz@gmail.com>
2022-11-11 18:14:48 +0300

    add active

--- js/app.js
+++ js/app.js
@@ -1,14 +1,38 @@
 //find element to append newer elements on
-const navBar = document.getElementById('navbar__list');
+const navBar = document.getElementById("navbar__list");
+
 // create menu
 for (let i = 1; i <= 4; i++) {
-    let newItem = document.createElement("li");
-    newItem.setAttribute("class", "navbar__item");
-    let newLink = document.createElement("a");
-    newLink.innerHTML = "Section " + i;
-    newLink.setAttribute("href", "#section" + i);
-    let section = document.getElementById("section" + i);
-    section.style.minHeight = "100vh";
-    newItem.appendChild(newLink);
-    navBar.appendChild(newItem);
+  let newItem = document.createElement("li");
+  newItem.setAttribute("class", "navbar__item");
+
+  let newLink = document.createElement("a");
+  newLink.innerHTML = "Section " + i;
+  newLink.setAttribute("href", "#section" + i);
+
+  let section = document.getElementById("section" + i);
+  section.style.minHeight = "100vh";
+
+  newItem.appendChild(newLink);
+  navBar.appendChild(newItem);
 }
+
+const links = document.querySelectorAll("li.navbar__item > a");
+
+links.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    //clear class att for each a link
+    links.forEach(function (el) {
+      if (el.classList.length > 0) {
+        el.className = " ";
+      }
+    });
+
+    // add class value 'active' for the clicked link
+    if (el.classList.length <= 0) {
+      el.classList.add("active");
+    } else {
+      el.className = " ";
+    }
+  });
+});
