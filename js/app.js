64a849bdebb66fbceaafbcb33380e145cc63fd3a
Nouf <nouffawazz@gmail.com>
2022-11-11 16:23:53 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 //find element to append newer elements on
-var navBar = document.getElementById('navbar__list');
+const navBar = document.getElementById('navbar__list');
 // create menu
 for (let i = 1; i <= 4; i++) {
-    const newItem = document.createElement("li");
-    newItem.setAttribute("class","navbar__item");
-    const newLink = document.createElement("a");
+    let newItem = document.createElement("li");
+    newItem.setAttribute("class", "navbar__item");
+    let newLink = document.createElement("a");
     newLink.innerHTML = "Section " + i;
-    newLink.setAttribute("href","#section"+i);
-    const section = document.getElementById("section"+i);
+    newLink.setAttribute("href", "#section" + i);
+    let section = document.getElementById("section" + i);
     section.style.minHeight = "100vh";
     newItem.appendChild(newLink);
     navBar.appendChild(newItem);
